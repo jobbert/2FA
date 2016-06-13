319067195b932ba9e6cc89bf8cce8acc1ea7d0c2
jobbert <jobpeters6@gmail.com>
2016-06-13 11:17:41 +0200

    necessary ^Cnfig files added

---
+++ app/AppKernel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+* @package phpBB Extension - Jobbert 2FA
+* @copyright (c) 2016 Job Peters
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+public function registerBundles()
+{
+    $bundles = array(
+        // ...
+        new Scheb\TwoFactorBundle\SchebTwoFactorBundle(),
+    );
+}
